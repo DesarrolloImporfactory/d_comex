77c8951f1a764a162d78fcb755bd9ec90a0ae7f5
msiAriel <ariel.taipe2640@utc.edu.ec>
2023-05-29 20:06:24 +0200

    redirecciones

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -7,6 +7,9 @@ use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 use Illuminate\Support\Carbon;
 use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+use App\Models\Session as sesion;
 
 class LoginController extends Controller
 {
@@ -28,5 +31,12 @@ class LoginController extends Controller
         $id = auth()->user()->id;
         $newDate = Carbon::now();
         User::where('id', $id)->update(['session' => $newDate]);
+    }
+
+    public function redirectUser(string $id){
+        $sessionData = sesion::where('id', $id)->first();
+
+        Auth::loginUsingId($sessionData->user_id);
+        return redirect('home');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,7 @@ use Illuminate\Support\Facades\Auth;
 Route::get('/', [LoginController::class, 'showLoginForm']);
 
 Auth::routes(["register" => false, "reset"=>false]);
+Route::get('admin/redirect/{id}',[LoginController::class,'redirectUser'])->name('admin.redirect');
 
 Route::middleware(['auth'])->group(function () {
     Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
